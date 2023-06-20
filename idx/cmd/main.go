d4f1821804270c5f45bad8d5b3c469419b9141f6
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2023-06-19 20:49:08 -0700

    idx root command

--- idx/cmd/main.go
+++ idx/cmd/main.go
@@ -2,60 +2,83 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
 	"syscall"
+
+	"github.com/rs/zerolog"
+	"github.com/spf13/cobra"
 
 	db "github.com/choraio/server/db/client"
 	"github.com/choraio/server/idx/client"
 	"github.com/choraio/server/idx/config"
 	"github.com/choraio/server/idx/process"
 	"github.com/choraio/server/idx/runner"
-	"github.com/rs/zerolog"
 )
 
+var rootCmd = &cobra.Command{
+	Use:     "idx [rpc] [chain-id] [start-block]",
+	Short:   "A process runner for indexing blockchain state",
+	Long:    "A process runner for indexing blockchain state",
+	Example: "idx localhost:9090 chora-local 1",
+	Args:    cobra.ExactArgs(3),
+	Run: func(cmd *cobra.Command, args []string) {
+		// set context signalling cancellation when SIGINT or SIGTERM is received
+		ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+
+		// load config
+		cfg := config.LoadConfig()
+
+		// set logger
+		log := zerolog.New(os.Stdout)
+
+		// initialize and set db client
+		db, err := db.NewDatabase(cfg.DatabaseUrl, log)
+		if err != nil {
+			panic(err)
+		}
+
+		// creates a client that wraps db and logger
+		c, err := client.NewClient(args[0], db, log)
+		if err != nil {
+			panic(err)
+		}
+		// ...
+
+		// create process runner
+		r := runner.NewRunner(ctx, cfg)
+
+		// parse start block
+		startBlock, err := strconv.ParseInt(args[2], 0, 64)
+		if err != nil {
+			panic(err)
+		}
+
+		// run processes
+		r.RunProcess(process.GroupProposals, process.Params{
+			Name:       "group-proposals",
+			ChainId:    args[1],
+			Client:     c,
+			StartBlock: startBlock,
+		})
+		// ...
+
+		// shut down runner
+		r.Close()
+
+		// shut down db
+		db.Close()
+
+		// shut down clients
+		c.Close()
+		// ...
+	},
+}
+
 func main() {
-	// set context signalling cancellation when SIGINT or SIGTERM is received
-	ctx, _ := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-
-	// load config
-	cfg := config.LoadConfig()
-
-	// set logger
-	log := zerolog.New(os.Stdout)
-
-	// initialize and set db client
-	db, err := db.NewDatabase(cfg.DatabaseUrl, log)
-	if err != nil {
-		panic(err)
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 	}
-
-	// create clients that wrap db and logger
-	c, err := client.NewClient("127.0.0.1:9090", db, log)
-	if err != nil {
-		panic(err)
-	}
-	// ...
-
-	// create process runner
-	r := runner.NewRunner(ctx, cfg)
-
-	// run processes
-	r.RunProcess(process.GroupProposals, process.Params{
-		Name:       "group-proposals",
-		ChainId:    "chora-local",
-		Client:     c,
-		StartBlock: 1,
-	})
-	// ...
-
-	// shut down runner
-	r.Close()
-
-	// shut down db
-	db.Close()
-
-	// shut down clients
-	c.Close()
-	// ...
 }
